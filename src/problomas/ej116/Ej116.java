3443a410aeb0b18fed77017b3cc271626586c96f
daniel <danimc2002.dmc@gmail.com>
2023-03-30 18:50:27 +0200

    Ejercicio 116 resulto y comentado

---
+++ src/problomas/ej116/Ej116.java
@@ -0,0 +1,18 @@
+package problomas.ej116;
+import java.util.Scanner;
+
+public class Ej116 {
+	public static void main(String[] args) {
+		// Declaramos e inicializamos un objeto Scanner y una variable
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+
+		// Ejecutamos "num" casos de prueba
+		for (int i = 0; i < num; i++) {
+			// Imprimimos lo que pide el enunciado
+			System.out.println("Hola mundo.");
+		}
+
+	}
+
+}
